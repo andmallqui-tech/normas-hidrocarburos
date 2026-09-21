7e96b4d192fb7e013515d2e4631fa198674069df
andmallqui-tech <andmallqui@gmail.com>
2026-09-21 12:26:50 -0500

    Add new mandatory keywords for TF-IDF processing

--- normas_github.py
+++ normas_github.py
@@ -327,7 +327,8 @@ SECTORES_EXCLUIR = set([normalizar_texto(x) for x in [
     'desarrollo social', 'trabajo', 'migraciones', 'cultura',
     'vivienda', 'comunicaciones', 'justicia', 'relaciones exteriores', 'midis', 'midagri','mdlp',
     'osinfor', 'senamhi', 'trabajo y promocion del empleo', 'sernanp', 'desarrollo agrario y riego',
-    'municipio', 'SENAMHI', 'SERNANP', 'SERVICIO NACIONAL DE AREAS NATURALES PROTEGIDAS POR EL ESTADO'
+    'municipio', 'SENAMHI', 'SERNANP', 'SERVICIO NACIONAL DE AREAS NATURALES PROTEGIDAS POR EL ESTADO',
+    'JURADO NACIONAL DE ELECCIONES', 'GOBIERNOS LOCALES', 'MUNICIPALIDAD DISTRITAL', 'GOBIERNO REGIONAL',
 ]])
 
 # Palabras obligatorias ampliadas → al menos una debe aparecer para pasar al TF-IDF
